10c54ce5d87ed7ed934ca72fc4d6970bc3e64f88
shiyu <shiyu@syswin.com>
2018-06-22 10:43:25 +0800

    nodejs+formidable 上传图片+预览

--- js/requestHandlers.js
+++ js/requestHandlers.js
@@ -1,5 +1,7 @@
 var exec = require('child_process').exec;
 var querystring = require("querystring");
+var fs = require("fs");
+var formidable = require("formidable");
 
 function start(response) {
     console.log("requestHandlersjs  Request handler 'start' was called.");
@@ -9,37 +11,68 @@ function start(response) {
         'charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
+        '<form action="/upload" method="post" enctype="multipart/form-data">' +
         '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
+        // '<input type="submit" value="Submit text" />' +
+        '<input type="file" name="upload" multiple="multiple">' +
+        '<input type="submit" value="提交" />' +
         '</form>' +
         '</body>' +
         '</html>';
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
-    // exec('ls -lah',function (error, stdout, stderr) {
-    //     response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    //     response.write(stdout);
-    //     response.end();
-    // })
-
-    // exec("find /",
-    //     {timeout: 10000, maxBuffer: 20000 * 1024},
-    //     function (error, stdout, stderr) {
-    //         response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    //         response.write(stdout+'6666666');
-    //         response.end();
-    //     });
-    // return 'start的内容';
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("requestHandlersjs  Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    response.write("Hello Upload " + postData + ", You've sent the text: " + querystring.parse(postData).text);
-    response.end();
+    if (request.url == '/upload' && request.method.toLowerCase() == 'post') {
+        //formidable模块的方法和属性
+        // Formidable.incomingForm()
+        //
+        // 可以通过该方法创建一个form表单
+        var form = new formidable.IncomingForm();
+        //通过uploadDir设置上传文件时临时文件存放的位置，默认上传的临时文件存放的位置为os.tmpDir();
+        form.uploadDir = 'tmp';
+        console.log("about to parse");
+        //parse方法解析Node.js中request请求中包含的form表单提交的数据。cb为处理请求的回调函数。
+        form.parse(request, function (error, fields, files) {
+            try {
+                console.log(request.method, files.upload.path);
+                // fs.renameSync(files.upload.path, './tmp/test.png');
+                fs.rename(files.upload.path, "./tmp/test.png", function () {
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.write("received image:<br/>");
+                    response.write("<img src='/show' />");
+                    response.end();
+                });
+            } catch (e) {
+                console.log('catch', e);
+            }
+        });
+    } else {
+        console.log('else', request.method, request.url)
+    }
+
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("./tmp/test.png", "binary", function (error, file) {
+        if (error) {
+            console.log('error', error, file)
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n33333");
+            response.end();
+        } else {
+            console.log('else ', file)
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- js/server.js
+++ js/server.js
@@ -14,25 +14,14 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log('serverjs Request for ' + pathname + ' received');
-        request.setEncoding("utf8");
-        request.addListener("data", function (postDataChunk) {
-            postData += postDataChunk;
-            console.log('Received POST data chunk ' + postDataChunk);
-        })
-        request.addListener("end",function () {
-            route(handle, pathname, response, postData);
-        })
-        // var content = route(handle, pathname);
-        //
-        // response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-        // response.write(content);
-        // response.end();
+
+        route(handle, pathname, response, request);
+
     }
 
-    http.createServer(onRequest).listen(8888);
+    http.createServer(onRequest).listen(8000);
     console.log('serverjs sever has started');
 }
 
